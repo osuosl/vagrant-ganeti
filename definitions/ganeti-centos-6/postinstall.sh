6702ef7b6cab0a5bc423b0a4d3f9c8871fcfc97a
Lance Albertson <lance@osuosl.org>
2012-05-28 23:31:28 -0700

    Use local mirror

--- definitions/ganeti-centos-6/postinstall.sh
+++ definitions/ganeti-centos-6/postinstall.sh
@@ -14,7 +14,7 @@ EOM
 cat > /etc/yum.repos.d/epel.repo << EOM
 [epel]
 name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
+baseurl=http://epel.osuosl.org/6/\$basearch
 enabled=1
 gpgcheck=0
 EOM
